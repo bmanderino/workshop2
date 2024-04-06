45021e330dd6d0680890f41c51a5648d60db9eb7
Brian Manderino <brian.manderino@gmail.com>
2024-04-06 12:24:23 -0700

    missing static url

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -133,6 +133,8 @@ if DEBUG:
 else:
     STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
+STATIC_URL = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
